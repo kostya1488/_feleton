fda573e46669b3e47fc9b28ce14621b4ccca9c0b
kostya <matvienkoigor956@gmail.com>
2020-09-30 18:35:21 +0300

    removed trash

--- js/cursor.js
+++ js/cursor.js
@@ -1,16 +1,41 @@
-// const cursor = document.querySelector('.cursor');
+const cursor = document.querySelector('.cursor2');
 
 // document.addEventListener('mousemove', e => {
 //     cursor.setAttribute("style", "top: " + (e.pageY - 2) + "px; left: " + (e.pageX - 2) + "px;")
 // })
 
-// $(document).click(() => {
-//     cursor.classList.add("expand");
-//     setTimeout(() => {
-//         cursor.classList.remove("expand");
-//     }, 500)
-// })
+$(document).mousedown(() => {
+    cursor.classList.add("expand");
+    // setTimeout(() => {
+    //     cursor.classList.remove("expand");
+    // }, 500)
+})
+$(document).mouseup(() => {
+    cursor.classList.remove("expand");
+    // setTimeout(() => {
+    //     cursor.classList.remove("expand");
+    // }, 500)
+})
+$('button, a').mouseover(() => {
+    cursor.classList.add("expand");
+    // setTimeout(() => {
+    //     cursor.classList.remove("expand");
+    // }, 500)
 
+})
+$('button, a').mouseout(() => {
+    cursor.classList.remove("expand");
+    // setTimeout(() => {
+    //     cursor.classList.remove("expand");
+    // }, 500)
+})
+
+// $('a').mouseout(() => {
+//     cursor.classList.add("expand");
+//     // setTimeout(() => {
+//     //     cursor.classList.remove("expand");
+//     // }, 500)
+// })
 document.getElementsByTagName("body")[0].addEventListener("mousemove", function(n) {
 
     t.style.left = (n.clientX) + "px",
